4b965827d89af67651a5f74387c8b75e0dbff3ec
yinuo2015 <yinuo2015@gmail.com>
2015-01-24 15:10:06 +0800

    界面更新并加入了游戏音效

--- Matchismo/Matchismo/ViewController.m
+++ Matchismo/Matchismo/ViewController.m
@@ -11,18 +11,26 @@
 #import "PlayingCardDeck.h"
 #import "PlayingCard.h"
 #import "CardMatchingGame.h"
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface ViewController ()
 @property (strong,nonatomic) CardMatchingGame *game;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
-- (IBAction)restartButton:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *modeSelector;
 @property (weak, nonatomic) IBOutlet UILabel *resultLabel;
-
+@property (strong,nonatomic) NSMutableArray *flipHistory;
+@property (weak, nonatomic) IBOutlet UISlider *historySlider;
 @end
 
 @implementation ViewController
+
+-(NSMutableArray *)flipHistory{
+	if (!_flipHistory) {
+		_flipHistory = [NSMutableArray array];
+	}
+	return _flipHistory;
+}
 
 - (CardMatchingGame *)game {
 	if (!_game) {
@@ -40,6 +48,7 @@
 
 - (IBAction)touchCardButton:(UIButton *)sender
 {
+	[self playClickSound];
 	NSUInteger chosenButtonIndex = [self.cardButtons indexOfObject:sender];
 	[self.game chooseCardAtIndex:chosenButtonIndex];
 	self.modeSelector.enabled = NO;
@@ -54,8 +63,8 @@
 		[cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
 		[cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
 		cardButton.enabled = !card.isMatched;
-		self.scoreLabel.text = [NSString stringWithFormat:@"Score:%ld",(long)self.game.score];
 	}
+	self.scoreLabel.text = [NSString stringWithFormat:@"Score:%ld",(long)self.game.score];
 	
 	if (self.game) {
 		NSString *description = @"";
@@ -72,7 +81,15 @@
 			description = [NSString stringWithFormat:@"%@ don't match %ld point penalty!",description,(long)-self.game.lastScore];
 		}
 		self.resultLabel.text = description;
+	
+		self.resultLabel.alpha = 1;
+		if (![description isEqualToString:@""] && ![[self.flipHistory lastObject] isEqualToString:description]) {
+			[self.flipHistory addObject:description];
+			[self setSliderRange];
+		}
+	
 	}
+	
 }
 
 -(NSString *)titleForCard:(Card*)card{
@@ -86,10 +103,37 @@
 - (IBAction)restartButton:(UIButton *)sender {
 	self.game = nil;
 	self.modeSelector.enabled = YES;
+	self.flipHistory = nil;
+	[self setSliderRange];
 	[self updateUI];
 }
 - (IBAction)changeModeSelector:(UISegmentedControl *)sender {
 		self.game.maxMatchingCards = [[sender titleForSegmentAtIndex:sender.selectedSegmentIndex] integerValue];
+}
+- (IBAction)changeSlider:(UISlider *)sender {
+	NSInteger sliderValue;
+	sliderValue = lroundf(self.historySlider.value);
+	[self.historySlider setValue:sliderValue animated:NO];
+	if ([self.flipHistory count]) {
+		self.resultLabel.alpha = (sliderValue+1 <[self.flipHistory count]) ? 0.6 : 1.0;
+		self.resultLabel.text = [self.flipHistory objectAtIndex:sliderValue];
+	}
+}
+
+-(void)setSliderRange{
+	NSInteger maxValue = [self.flipHistory count]-1;
+	self.historySlider.maximumValue = maxValue;
+	[self.historySlider setValue:maxValue animated:YES];
+}
+
+-(void)playClickSound{
+	
+	SystemSoundID soundID;
+	
+	NSString *soundPath = [[NSBundle mainBundle] pathForResource:@"click" ofType:@"wav"];
+	CFURLRef soundURL = (__bridge CFURLRef)[NSURL fileURLWithPath:soundPath];
+	AudioServicesCreateSystemSoundID(soundURL, &soundID);
+	AudioServicesPlaySystemSound(soundID);
 }
 
 
